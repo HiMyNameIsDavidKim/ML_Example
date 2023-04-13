f7705a7f2e405583c31bbf4b65bbb067fbb1da3b
HiMyNameIsDavidKim <rkfka1401@gmail.com>
2023-04-13 12:05:45 +0900

    4/13 study update

--- CV/timm_vit_cifar100.py
+++ CV/timm_vit_cifar100.py
@@ -4,6 +4,8 @@ import torch.nn as nn
 import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
+from torch.optim import Adam
+from torch.optim.lr_scheduler import CosineAnnealingLR
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from tqdm import tqdm
@@ -13,7 +15,7 @@ model_path = './save/timm_ViT_Cifar100.pt'
 # device = 'mps'
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 BATCH_SIZE = 512
-NUM_EPOCHS = 10
+NUM_EPOCHS = 10000
 NUM_WORKERS = 2
 LEARNING_RATE = 0.001
 
@@ -38,6 +40,9 @@ testloader = DataLoader(testset, batch_size=BATCH_SIZE, shuffle=True, num_worker
 class ViTCifar100Model(object):
     def __init__(self):
         self.model = None
+        self.optimizer = None
+        self.scheduler = None
+        self.epoch = 0
 
     def process(self):
         self.build_modeL()
@@ -52,7 +57,8 @@ class ViTCifar100Model(object):
     def train_model(self):
         model = self.model.to(device)
         criterion = nn.CrossEntropyLoss()
-        optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+        optimizer = Adam(model.parameters(), lr=LEARNING_RATE)
+        scheduler = CosineAnnealingLR(optimizer, T_max=NUM_EPOCHS, eta_min=0, last_epoch=-1)
 
         for epoch in range(NUM_EPOCHS):
             running_loss = 0.0
@@ -71,26 +77,42 @@ class ViTCifar100Model(object):
                 if i % 100 == 99:
                     print(f'[Epoch {epoch + 1}, Batch {i + 1:5d}] loss: {running_loss / 100:.3f}')
                     running_loss = 0.0
+            if epoch % 10 == 0:
+                self.epoch = epoch
+                self.model = model
+                self.optimizer = optimizer
+                self.scheduler = scheduler
+                self.save_model()
+            scheduler.step()
         print('****** Finished Training ******')
 
-        self.model = model
-
     def save_model(self):
-        torch.save(self.model, model_path)
+        checkpoint = {
+            'epoch': self.epoch,
+            'model': self.model.state_dict(),
+            'optimizer': self.optimizer.state_dict(),
+            'scheduler': self.scheduler.state_dict(),
+        }
+        torch.save(checkpoint, model_path)
+        print(f"****** Model checkpoint saved at epoch {self.epoch} ******")
 
     def eval_model(self):
-        model = torch.load(model_path).to(device)
+        checkpoint = torch.load(model_path)
+        self.model.load_state_dict(checkpoint['model'])
+        self.optimizer.load_state_dict(checkpoint['optimizer'])
+        self.model.eval()
+
         correct = 0
         total = 0
         with torch.no_grad():
             for data in testloader:
                 images, labels = data
                 images, labels = images.to(device), labels.to(device)
-                outputs = model(images)
+                outputs = self.model(images)
                 _, predicted = torch.max(outputs.data, 1)
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
-        print(f'Accuracy of the network on the {len(testset)} test images: {100 * correct / total:.2f} %')
+        print(f'Accuracy {len(testset)} test images: {100 * correct / total:.2f} %')
 
 
 if __name__ == "__main__":
