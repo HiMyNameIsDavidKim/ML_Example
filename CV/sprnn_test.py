0d2223378ea10d9767afc01a4fa7cf2c5f71d003
HiMyNameIsDavidKim <rkfka1401@gmail.com>
2023-09-06 17:07:00 +0900

    9/6 study update

--- CV/sprnn_test.py
+++ CV/sprnn_test.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import numpy as np
 
-width, height = 1080, 1920
+width, height = 10, 10
 
 def gray(level):
     image = np.full((height, width), level, dtype=np.uint8)
@@ -10,19 +10,35 @@ def gray(level):
 def save_img(image, path):
     image = Image.fromarray(image, 'L')
     image.save(path)
+    # dpi = 460
+    # image = image.resize((int(width * dpi / 25.4), int(height * dpi / 25.4)))
+    # image.save(path, dpi=(dpi, dpi))
 
-def quarter(image):
+def dim2(image):
     image[1::2, ::] = 0
     image[::, 1::2] = 0
+    print(image)
+    return image
+
+def dim3(image):
+    image[1::3, ::] = 0
+    image[2::3, ::] = 0
+    image[::, 1::3] = 0
+    image[::, 2::3] = 0
     return image
 
 
 if __name__ == '__main__':
-    gray46 = gray(46)
-    save_img(gray46, './save/gray46_full.png')
+    level = 46
+    gray46 = gray(level)
+    # save_img(gray46, f'./save/gray{level}_full.png')
 
-    gray186 = gray(186)
-    gray186_q = quarter(gray186)
-    save_img(gray186_q, './save/gray186_quarter.png')
+    level = 186
+    gray186 = gray(level)
+    gray186_dim2 = dim2(gray186)
+    # save_img(gray186_dim2, f'./save/gray{level}_dim2.png')
 
-
+    level = 186
+    gray186 = gray(level)
+    gray186_dim3 = dim3(gray186)
+    # save_img(gray186_dim3, f'./save/gray{level}_dim3.png')
