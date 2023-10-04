fbf01cfaceb5ab015a224f2f0b0f0142e387c6b0
HiMyNameIsDavidKim <rkfka1401@gmail.com>
2023-10-04 20:21:19 +0900

    10/4 study update

---
+++ NLP/chatgpt_part1/part02/chapter03/generation_0.py
@@ -0,0 +1,26 @@
+import os
+
+import openai
+from dotenv import load_dotenv
+
+file = open(r"/Users/davidkim/security/openai.txt", "r", encoding='UTF8')
+data = file.read()
+KEY_NLP = str(data)
+file.close()
+
+load_dotenv()
+
+openai.api_key = os.getenv(KEY_NLP)
+
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "What can you do?"},
+    ],
+    temperature=1,
+    n=4,
+)
+
+for res in response.choices:
+    print(res.message)
