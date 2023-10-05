fb29eec9284af0060110d907fffa04719fa6b2e4
HiMyNameIsDavidKim <rkfka1401@gmail.com>
2023-10-05 17:22:50 +0900

    10/5 study update

--- CV/sprnn_test.py
+++ CV/sprnn_test.py
@@ -3,21 +3,27 @@ from PIL import Image
 import numpy as np
 import random
 
-width, height = 1179, 2556  # 30 and 59
+# width, height = 1179, 2556  # 30 and 59
 # width, height = 512, 260
-# width, height = 1080, 2340  # 30 and 65
+width, height = 1080, 2340  # 30 and 65
 
 
-def gray(level):
+def gray(level, greenish=False):
     color = (level, level, level)
+    if greenish:
+        color = (level, level+7, level)
     image = np.full((height, width, 3), color, dtype=np.uint8)
     return image
 
-def green(level):
-    color = (0, level, 0)
+def single(level, str_color):
+    if str_color =='green':
+        color = (0, level, 0)
+    elif str_color =='red':
+        color = (0, 0, level)
+    elif str_color =='blue':
+        color = (level, 0, 0)
     image = np.full((height, width, 3), color, dtype=np.uint8)
     return image
-
 
 def save_img(image, path):
     cv2.imwrite(path, image)
@@ -37,9 +43,9 @@ def dim3(image):
     image[::, 2::3] = 0
     return image
 
-def apply_noise(image, intensity, green=False):
+def apply_noise(image, intensity, single=False):
     noise = np.random.uniform(-intensity, intensity, size=image.shape)
-    if green:
+    if single:
         noise[:,:,:1] = 0
         noise[:, :, -1:] = 0
     image = image - noise
@@ -53,16 +59,20 @@ def half_half(image_1, image_2):
 
 
 if __name__ == '__main__':
-    # for i in [30]:
-    #     level = i
-    #     gray_ptn = gray(level)
-    #     save_img(gray_ptn, f'./save/gray{level}_full.png')
+    # 그레이 full 패턴
+    for i in [30]:
+        level = i
+        gray_ptn = gray(level)
+        save_img(gray_ptn, f'./save/gray{level}_full.png')
 
-    # level = 186
-    # gray_ptn = gray(level)
-    # gray_ptn_dim2 = dim2(gray_ptn)
-    # save_img(gray_ptn_dim2, f'./save/gray{level}_dim2.png')\
+    # 그레이 1/4 패턴
+    for i in [65]:
+        level = i
+        gray_ptn = gray(level)
+        gray_ptn_dim2 = dim2(gray_ptn)
+        save_img(gray_ptn_dim2, f'./save/gray{level}_dim2.png')\
 
+    # 그레이 full 패턴 + 노이즈
     # for i in [30]:
     #     level = i
     #     gray_ptn = gray(level)
@@ -70,37 +80,42 @@ if __name__ == '__main__':
     #     gray_ptn = apply_noise(gray_ptn, intensity)
     #     save_img(gray_ptn, f'./save/gray{level}_full_noise{intensity}.png')
 
-    for i in [30]:
-        level = i
-        green_ptn = green(level)
-        save_img(green_ptn, f'./save/green{level}_full.png')
+    # 그레이 반반 패턴 (1 is full, 2 is 1/4)
+    level_1 = 30
+    level_2 = 65
+    single_ptn = gray(level_1)
+    single_ptn_dim = dim2(gray(level_2, greenish=True))
+    half_half_ptn = half_half(single_ptn, single_ptn_dim)
+    save_img(half_half_ptn, f'./save/gray{level_1}_full_and_gray{level_2}_dim2.png')
 
-    for i in [59]:
-        level = i
-        green_ptn = green(level)
-        green_ptn_dim = dim2(green_ptn)
-        save_img(green_ptn_dim, f'./save/green{level}_dim2.png')
+    str_color = 'blue'
 
+    # 싱글 full 패턴
+    # for i in [30]:
+    #     level = i
+    #     single_ptn = single(level, str_color)
+    #     save_img(single_ptn, f'./save/{str_color}{level}_full.png')
+
+    # 싱글 1/4 패턴
     # for i in [65]:
     #     level = i
-    #     green_ptn = green(level)
-    #     green_ptn_dim = dim2(green_ptn)
+    #     single_ptn = single(level, str_color)
+    #     single_ptn_dim = dim2(single_ptn)
+    #     save_img(single_ptn_dim, f'./save/{str_color}{level}_dim2.png')
+
+    # 싱글 full 패턴 + 노이즈
+    # for i in [65]:
+    #     level = i
+    #     single_ptn = single(level)
+    #     single_ptn_dim = dim2(single_ptn)
     #     intensity = 4
-    #     green_ptn_noise = apply_noise(green_ptn_dim, intensity, green=True)
-    #     save_img(green_ptn_noise, f'./save/green{level}_dim2_noise{intensity}.png')
+    #     single_ptn_noise = apply_noise(single_ptn_dim, intensity, single=True)
+    #     save_img(single_ptn_noise, f'./save/single{level}_dim2_noise{intensity}.png')
 
-    # # 1 is noise, 2 is good
-
-    level_1 = 30
-    level_2 = 60
-    green_ptn = green(level_1)
-    green_ptn_dim = dim2(green(level_2))
-    half_half_ptn = half_half(green_ptn, green_ptn_dim)
-    save_img(half_half_ptn, f'./save/green{level_1}_full_and_green{level_2}_dim2.png')
-
-    # level_1 = 65
+    # 싱글 반반 패턴 (1 is full, 2 is 1/4)
+    # level_1 = 30
     # level_2 = 65
-    # green_ptn_noise_dim = dim2(apply_noise(green(level_1), 4, green=True))
-    # green_ptn_dim = dim2(green(level_2))
-    # half_half_ptn = half_half(green_ptn_noise_dim, green_ptn_dim)
-    # save_img(half_half_ptn, f'./save/green{level_1}_noise4_dim2_and_green{level_2}_dim2.png')
+    # single_ptn = single(level_1, str_color)
+    # single_ptn_dim = dim2(single(level_2, str_color))
+    # half_half_ptn = half_half(single_ptn, single_ptn_dim)
+    # save_img(half_half_ptn, f'./save/{str_color}{level_1}_full_and_{str_color}{level_2}_dim2.png')
