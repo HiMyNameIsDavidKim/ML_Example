365ac1609e2623005e0812cd906e5dfa4a6de9be
HiMyNameIsDavidKim <rkfka1401@gmail.com>
2023-11-06 16:38:44 +0900

    11/6 study update

--- CV/facebook_mae_imagenet.py
+++ CV/facebook_mae_imagenet.py
@@ -1,3 +1,6 @@
+import math
+import sys
+
 import torch
 import torchvision
 import torch.utils.data as data
@@ -12,7 +15,10 @@ from functools import partial
 import facebook_vit
 from mae_util import interpolate_pos_embed
 from timm.models.layers import trunc_normal_
-from CV.facebook_mae import MaskedAutoencoderViT
+
+import facebook_mae
+import mae_misc as misc
+from mae_misc import NativeScalerWithGradNormCount as NativeScaler
 
 gpu_ids = []
 device_names = []
@@ -35,7 +41,8 @@ NUM_EPOCHS = 100  # 100
 WARMUP_EPOCHS = 5  # 5
 NUM_WORKERS = 2
 LEARNING_RATE = 3.125e-05  # paper: 1e-03 -> implementation: 5e-04
-pre_model_path = './data/mae_checkpoint/mae_finetuned_vit_base_given.pth'
+pre_model_path = f'./save/mae_base_i2012_ep{NUM_EPOCHS}_lr{LEARNING_RATE}.pt'
+load_model_path = './data/mae_checkpoint/mae_finetuned_vit_base_given.pth'
 fine_model_path = f'./save/mae_vit_base_i2012_ep{NUM_EPOCHS}_lr{LEARNING_RATE}.pt'
 dynamic_model_path = f'./save/mae_vit_base_i2012_ep'
 
@@ -56,7 +63,7 @@ test_set = torchvision.datasets.ImageFolder('./data/ImageNet/val', transform=tra
 test_loader = data.DataLoader(test_set, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS)
 
 
-class FineTunner(object):
+class FineTuner(object):
     def __init__(self):
         self.model = None
         self.optimizer = None
@@ -71,16 +78,13 @@ class FineTunner(object):
         self.save_model()
 
     def build_model(self, load):
-        self.model = facebook_vit.__dict__['vit_base_patch16'](
-            num_classes=1000,
-            drop_path_rate=0.1,
-            )
+        self.model = facebook_vit.__dict__['vit_base_patch16'](num_classes=1000, drop_path_rate=0.1)
         print(f'Parameter: {sum(p.numel() for p in self.model.parameters() if p.requires_grad)}')
         self.optimizer = SGD(self.model.parameters(), lr=0)
         self.scheduler = CosineAnnealingLR(self.optimizer, T_max=NUM_EPOCHS)
 
         if load:
-            checkpoint = torch.load(pre_model_path)
+            checkpoint = torch.load(load_model_path)
             checkpoint_model = checkpoint['model']
             state_dict = self.model.state_dict()
             for k in ['head.weight', 'head.bias']:
@@ -93,7 +97,7 @@ class FineTunner(object):
             trunc_normal_(self.model.head.weight, std=2e-5)
             self.model.to(device)
 
-            if 'given' not in str(pre_model_path):
+            if 'given' not in str(load_model_path):
                 self.epochs = checkpoint['epochs']
                 self.losses = checkpoint['losses']
                 self.accuracies = checkpoint['accuracies']
@@ -170,6 +174,95 @@ class FineTunner(object):
         print(f"****** Model checkpoint saved at epochs {self.epochs[-1]} ******")
 
 
+class PreTrainer(object):
+    def __init__(self):
+        self.model = None
+        self.optimizer = None
+        self.scheduler = None
+        self.epochs = [0]
+        self.losses = [0]
+        self.accuracies = [0]
+
+    def process(self, load=False):
+        self.build_model(load)
+        self.pretrain_model()
+        self.save_model()
+
+    def build_model(self, load):
+        self.model = facebook_mae.__dict__['mae_vit_base_patch16'](norm_pix_loss=False).to(device)
+        print(f'Parameter: {sum(p.numel() for p in self.model.parameters() if p.requires_grad)}')
+        self.optimizer = SGD(self.model.parameters(), lr=0)
+        self.scheduler = CosineAnnealingLR(self.optimizer, T_max=NUM_EPOCHS)
+
+        if load:
+            checkpoint = torch.load(load_model_path)
+            self.model.load_state_dict(checkpoint['model'])
+            if 'given' not in str(load_model_path):
+                self.epochs = checkpoint['epochs']
+                self.losses = checkpoint['losses']
+                self.accuracies = checkpoint['accuracies']
+            print(f'Parameter: {sum(p.numel() for p in self.model.parameters() if p.requires_grad)}')
+            print(f'Epoch: {self.epochs[-1]}')
+            print(f'****** Reset epochs and losses ******')
+            self.epochs = []
+            self.losses = []
+            self.accuracies = []
+
+    def pretrain_model(self):
+        model = self.model.train()
+        optimizer = AdamW(model.parameters(), lr=LEARNING_RATE, betas=(0.9, 0.95))
+        scheduler = CosineAnnealingLR(optimizer, T_max=NUM_EPOCHS)
+        loss_scaler = NativeScaler()
+
+        for epoch in range(NUM_EPOCHS):
+            if epoch < WARMUP_EPOCHS:
+                lr_warmup = ((epoch + 1) / WARMUP_EPOCHS) * LEARNING_RATE
+                for param_group in optimizer.param_groups:
+                    param_group['lr'] = lr_warmup
+                if epoch + 1 == WARMUP_EPOCHS:
+                    scheduler = CosineAnnealingLR(optimizer, T_max=NUM_EPOCHS)
+            print(f"epoch {epoch + 1} learning rate : {optimizer.param_groups[0]['lr']}")
+            running_loss = 0.0
+            saving_loss = 0.0
+            for i, data in tqdm_notebook(enumerate(train_loader, 0), total=len(train_loader)):
+                samples, _ = data
+                samples = samples.to(device, non_blocking=True)
+
+                optimizer.zero_grad()
+
+                loss, _, _ = model(samples, mask_ratio=.75)
+                loss_scaler(loss, optimizer, parameters=model.parameters(), update_grad=True)  # include backward()
+
+                running_loss += loss.item()
+                saving_loss += loss.item()
+
+                if i % 100 == 99:
+                    print(f'[Epoch {epoch}, Batch {i + 1:5d}] loss: {running_loss / 100:.3f}, acc: {correct/total*100:.2f} %')
+                    running_loss = 0.0
+                if i % 1000 == 999:
+                    self.epochs.append(epoch + 1)
+                    self.losses.append(saving_loss/1000)
+                    saving_loss = 0.0
+            self.model = model
+            self.optimizer = optimizer
+            self.scheduler = scheduler
+            self.save_model()
+            scheduler.step()
+        print('****** Finished Fine-tuning ******')
+
+    def save_model(self):
+        checkpoint = {
+            'model': self.model.state_dict(),
+            'optimizer': self.optimizer.state_dict(),
+            'scheduler': self.scheduler.state_dict(),
+            'epochs': self.epochs,
+            'losses': self.losses,
+        }
+        torch.save(checkpoint, fine_model_path)
+#         torch.save(checkpoint, dynamic_model_path+str(self.epochs[-1])+f'_lr{LEARNING_RATE}.pt')
+        print(f"****** Model checkpoint saved at epochs {self.epochs[-1]} ******")
+
+
 if __name__ == '__main__':
-    trainer = FineTunner()
+    trainer = FineTuner()
     trainer.process(load=True)
