f546b887d0d0ef658cb1577dc6d69781ead35417
HiMyNameIsDavidKim <rkfka1401@gmail.com>
2023-11-17 15:45:41 +0900

    11/17 study update

--- CV/zJustTest.py
+++ CV/zJustTest.py
@@ -4,7 +4,6 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchvision import transforms, datasets
 
-# 이미지 세그멘테이션 모델 정의
 class SimpleSegmentationModel(nn.Module):
     def __init__(self, num_classes):
         super(SimpleSegmentationModel, self).__init__()
@@ -21,16 +20,13 @@ class SimpleSegmentationModel(nn.Module):
         x = self.decoder(x)
         return x
 
-# 데이터 로딩 및 데이터 전처리
 train_dataset = datasets.Cityscapes(root='./data/cityscapes', split='train', mode='fine', target_type='semantic')
 train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
 
-# 모델 초기화 및 손실 함수 및 최적화 함수 설정
 model = SimpleSegmentationModel(num_classes=19)  # Cityscapes dataset has 19 classes
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-# 학습 루프
 num_epochs = 10
 for epoch in range(num_epochs):
     model.train()
