fc1800eaae66184e74bcf8133dd2b6b6abc8eb17
HiMyNameIsDavidKim <rkfka1401@gmail.com>
2023-11-22 17:08:36 +0900

    11/21 study update

--- CV/util/custom_activation.py
+++ CV/util/custom_activation.py
@@ -2,19 +2,19 @@ import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
 
-class MaskLayer(nn.Module):
-    def forward(self, x, masking):
+class MaskingLayer(nn.Module):
+    def forward(self, x, masking=(10, 30)):
         (masking_low, masking_high) = masking
-        return torch.where((x < masking_low) | (masking_high < x), x, torch.tensor(0, dtype=x.dtype))
+        return torch.where((x < masking_low) | (masking_high < x), x, torch.tensor(0, dtype=x.dtype).to(x.device))
 
 
 class CustomModel(nn.Module):
     def __init__(self):
         super(CustomModel, self).__init__()
-        self.mask_layer = MaskLayer()
+        self.masking_layer = MaskingLayer()
 
     def forward(self, x):
-        x = self.mask_layer(x, masking=(0, 1))
+        x = self.masking_layer(x, masking=(0, 1))
         return x
 
 
