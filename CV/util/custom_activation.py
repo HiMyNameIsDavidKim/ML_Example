bafdcf445c58babea8deadb30b5b8f7bfa89614e
HiMyNameIsDavidKim <rkfka1401@gmail.com>
2024-01-03 12:24:43 +0900

    1/3 study update

--- CV/util/custom_activation.py
+++ CV/util/custom_activation.py
@@ -6,18 +6,19 @@ import matplotlib.pyplot as plt
 from PIL import Image
 
 class MaskingLayer(nn.Module):
-    def __init__(self):
+    def __init__(self, masking=(10, 30)):
         super(MaskingLayer, self).__init__()
+        self.masking = masking
+        (masking_low, masking_high) = masking
         scale_array = np.array(range(256), dtype=np.uint8)
         scale_image = Image.fromarray(scale_array)
         scale_tensor = transforms.ToTensor()(scale_image)
         self.scale_tensor_array = scale_tensor.numpy().flatten()
+        self.masking_low = self.scale_tensor_array[masking_low]
+        self.masking_high = self.scale_tensor_array[masking_high]
 
-    def forward(self, x, masking=(10, 30)):
-        (masking_low, masking_high) = masking
-        masking_low = self.scale_tensor_array[masking_low]
-        masking_high = self.scale_tensor_array[masking_high]
-        return torch.where((x < masking_low) | (masking_high < x), x, torch.tensor(0, dtype=x.dtype).to(x.device))
+    def forward(self, x):
+        return torch.where((x < self.masking_low) | (self.masking_high < x), x, torch.tensor(0, dtype=x.dtype).to(x.device))
 
 
 class TestModel(nn.Module):
@@ -35,17 +36,15 @@ if __name__ == '__main__':
     input_data = torch.rand(size=(1, 100))
     output_data = model(input_data)
 
-    print(output_data)
-
-    plt.figure(figsize=(12, 6))
-    plt.subplot(1, 2, 1)
-    plt.hist(input_data.numpy().flatten(), bins=50, color='blue', alpha=0.7)
+    plt.figure(figsize=(12, 12))
+    plt.subplot(2, 1, 1)
+    plt.hist(input_data.numpy().flatten(), color='blue', alpha=0.7, bins=50)
     plt.title('Input Data Distribution')
     plt.xlabel('Values')
     plt.ylabel('Frequency')
 
-    plt.subplot(1, 2, 2)
-    plt.hist(output_data.detach().numpy().flatten(), bins=50, color='orange', alpha=0.7)
+    plt.subplot(2, 1, 2)
+    plt.hist(output_data.detach().numpy().flatten(), color='red', alpha=0.7, bins=50)
     plt.title('Model Output Distribution')
     plt.xlabel('Values')
     plt.ylabel('Frequency')
