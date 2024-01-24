666cdbfa53a79e9679db605ec33f4250680c3cb2
HiMyNameIsDavidKim <rkfka1401@gmail.com>
2024-01-24 17:18:25 +0900

    1/24 study update

---
+++ CV/puzzle_res50.py
@@ -0,0 +1,184 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from matplotlib import pyplot as plt
+from torch.optim.lr_scheduler import CosineAnnealingLR
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+from torchvision.models import resnet50
+import math
+
+from CV.util.tester import visualDoubleLoss
+
+device = 'cpu'
+lr = 3e-05
+batch_size = 64
+NUM_EPOCHS = 20
+
+
+transform = transforms.Compose([
+    transforms.Pad(padding=3),
+    transforms.CenterCrop(30),
+    transforms.Grayscale(num_output_channels=3),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+
+train_dataset = datasets.CIFAR10(root='./data', train=True, transform=transform, download=True)
+test_dataset = datasets.CIFAR10(root='./data', train=False, transform=transform, download=True)
+
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+
+class PuzzleCNNCoord(nn.Module):
+    def __init__(self, num_puzzle=9, size_puzzle=10, threshold=0.8):
+        super(PuzzleCNNCoord, self).__init__()
+        self.num_puzzle = num_puzzle
+        self.size_puzzle = size_puzzle
+        self.map_values = []
+        self.map_coord = None
+        self.min_dist = 0
+        self.threshold = threshold
+        resnet = resnet50(pretrained=True)
+        resnet_output_size = resnet.fc.in_features
+        self.resnet_features = nn.Sequential(*list(resnet.children())[:-2])
+        self.fc1 = nn.Linear(resnet_output_size, 4096)
+        self.fc2 = nn.Linear(4096, 4096)
+        self.fc3 = nn.Linear(4096, 4096)
+        self.fc4 = nn.Linear(4096, 4096)
+        self.fc5 = nn.Linear(4096, self.num_puzzle * 2)
+
+    def random_shuffle(self, x):
+        N, C, H, W = x.shape
+        p = self.size_puzzle
+        n = int(math.sqrt(self.num_puzzle))
+
+        noise = torch.rand(N, self.num_puzzle, device=x.device)
+        ids_shuffles = torch.argsort(noise, dim=1)
+        ids_restores = torch.argsort(ids_shuffles, dim=1)
+
+        for i, (img, ids_shuffle) in enumerate(zip(x, ids_shuffles)):
+            pieces = [img[:, i:i + p, j:j + p] for i in range(0, H, p) for j in range(0, W, p)]
+            shuffled_pieces = [pieces[idx] for idx in ids_shuffle]
+            shuffled_img = [torch.cat(row, dim=2) for row in [shuffled_pieces[i:i+n] for i in range(0, len(shuffled_pieces), n)]]
+            shuffled_img = torch.cat(shuffled_img, dim=1)
+            x[i] = shuffled_img
+
+        start, end = 0, n
+        self.min_dist = (end-start)/n
+        self.map_values = list(torch.arange(start, end, self.min_dist))
+        self.map_coord = torch.tensor([(i, j) for i in self.map_values for j in self.map_values])
+
+        coord_shuffles = torch.zeros([N, self.num_puzzle, 2])
+        coord_restores = torch.zeros([N, self.num_puzzle, 2])
+        for i, (ids_shuffle, ids_restore) in enumerate(zip(ids_shuffles, ids_restores)):
+            coord_shuffles[i] = self.map_coord[ids_shuffle]
+            coord_restores[i] = self.map_coord[ids_restore]
+
+        return x, coord_restores.to(x.device)
+
+    def forward_loss_dist(self, x):
+        N, n, c = x.shape
+        distances = torch.zeros((N, n, n), device=x.device)
+        for batch in range(N):
+            distances[batch] = torch.cdist(x[batch], x[batch]) + torch.eye(self.num_puzzle, device=x.device)
+        loss_dist = torch.sum(torch.relu((self.threshold * self.min_dist) - distances))
+        return loss_dist
+
+    def mapping(self, target):
+        diff = torch.abs(target.unsqueeze(3) - torch.tensor(self.map_values, device=target.device))
+        min_indices = torch.argmin(diff, dim=3)
+        target[:] = min_indices
+        return target
+
+    def forward(self, x):
+        x, target = self.random_shuffle(x)
+        x = self.resnet_features(x)
+        x = F.adaptive_avg_pool2d(x, (1, 1))
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+        x = self.fc5(x)
+        x = x.view(-1, self.num_puzzle, 2)
+        loss_dist = self.forward_loss_dist(x)
+        return x, target, loss_dist
+
+
+if __name__ == '__main__':
+    model = PuzzleCNNCoord().to(device)
+    criterion = nn.SmoothL1Loss()
+    optimizer = optim.AdamW(model.parameters(), lr=lr, weight_decay=0.05)
+    scheduler = CosineAnnealingLR(optimizer, T_max=NUM_EPOCHS)
+
+    ls = []
+    ls_t = []
+    for epoch in range(NUM_EPOCHS):
+        model.train()
+
+        running_loss = 0.
+        running_loss_t = 0.
+        for batch_idx, (inputs, _) in enumerate(train_loader):
+            inputs = inputs.to(device)
+
+            outputs, labels, loss_dist = model(inputs)
+
+            optimizer.zero_grad()
+
+            loss_coord = criterion(outputs, labels)
+            loss = loss_coord + loss_dist
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss_coord.item()
+            running_loss_t += loss.item()
+
+            inter = 30
+            if batch_idx % inter == inter-1:
+                print(f'[Epoch {epoch+1}] [Batch {batch_idx+1}] Loss: {running_loss/inter:.4f}')
+                print(f'[Epoch {epoch+1}] [Batch {batch_idx+1}] Total Loss: {running_loss_t/inter:.4f}')
+                ls.append(running_loss/inter)
+                ls_t.append(running_loss_t/inter)
+                running_loss = 0.
+                running_loss_t = 0.
+        scheduler.step()
+        visualDoubleLoss(ls, ls_t)
+
+        model.eval()
+
+        total = 0
+        diff = 0
+        correct = 0
+        with torch.no_grad():
+            for inputs, _ in test_loader:
+                inputs = inputs.to(device)
+
+                outputs, labels, _ = model(inputs)
+
+                pred = outputs
+                total += labels.size(0)
+                diff += (torch.dist(pred, labels)).sum().item()
+                pred_ = model.mapping(pred)
+                labels_ = model.mapping(labels)
+                correct += (pred_ == labels_).all(dim=2).sum().item()
+
+        print(f'[Epoch {epoch+1}] Avg diff on the test set: {diff / total:.2f}')
+        print(f'[Epoch {epoch+1}] Accuracy on the test set: {100 * correct / (total*labels.size(1)):.2f}%')
+        torch.set_printoptions(precision=2)
+        total = labels.size(1)
+        correct = (pred_[0] == labels_[0]).all(dim=1).sum().item()
+        print(torch.cat((pred_[0], labels_[0]), dim=1))
+        print(f'Accuracy: {100 * correct / (labels.size(1)):.2f}')
+
+        checkpoint = {
+            'model': model.state_dict(),
+            'optimizer': optimizer.state_dict(),
+            'losses': ls,
+            'total_losses': ls_t,
+        }
+        torch.save(checkpoint, f'./save/puzzle_res50_ep{NUM_EPOCHS}_lr{lr}_b{batch_size}.pt')
+        print(f"### Model checkpoint saved at epoch {epoch+1} ###")
--- CV/spr_transformer.py
+++ CV/spr_transformer.py
@@ -4,38 +4,134 @@ import torch
 import torch.nn as nn
 
 import timm.models.vision_transformer
+from timm.models.vision_transformer import PatchEmbed, Block
+
+from sprt_util import get_2d_sincos_pos_embed
 
 
 # --------------------------------------------------------
-# References:
 # timm: https://github.com/rwightman/pytorch-image-models/tree/master/timm
 # --------------------------------------------------------
+# SPRNN
+# img_size=192, patch_size=16, in_chans=2
+# input = [batch, 2, 192, 192]
+# dim = [batch, 256, 192, 192]
+# MaxPool2d 1/2
+# output = [batch 1, 96, 96]
+# --------------------------------------------------------
+# SPRT
+# img_size=192, patch_size=16, in_chans=2
+# input = [batch, 2, 192, 192]
+# encoder_dim = [batch, 768, 192, 192]
+# decoder_dim = [batch, 512, 192, 192]
+# output = [batch 1, 96, 96]
+# --------------------------------------------------------
 
 
-class SPRTransformer(timm.models.vision_transformer.VisionTransformer):
-    def __init__(self, **kwargs):
-        super(SPRTransformer, self).__init__(**kwargs)
+class SPRTransformer(nn.Module):
+    def __init__(self, img_size=192, patch_size=16, in_chans=2, embed_dim=768, depth=12, num_heads=12,
+                 out_patch_size=8, out_chans=1, decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
+                 mlp_ratio=4., norm_layer=nn.LayerNorm, norm_pix_loss=False):
+        super().__init__()
+        # --------------------------------------------------------------------------
+        # encoder specifics
+        self.patch_embed = PatchEmbed(img_size, patch_size, in_chans, embed_dim)
+        num_patches = self.patch_embed.num_patches
 
-    def forward_features(self, x):
-        B = x.shape[0]
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
+        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches + 1, embed_dim),
+                                      requires_grad=False)  # fixed sin-cos embedding
+
+        self.blocks = nn.ModuleList([
+            Block(embed_dim, num_heads, mlp_ratio, qkv_bias=True, qk_scale=None, norm_layer=norm_layer)
+            for i in range(depth)])
+        self.norm = norm_layer(embed_dim)
+        # --------------------------------------------------------------------------
+
+        # --------------------------------------------------------------------------
+        # decoder specifics 여기 패치 풀어헤치는 과정 크기 언제 바꿀지? 바꾸긴 바꿀지? 고민하기
+        self.decoder_embed = nn.Linear(embed_dim, decoder_embed_dim, bias=True)
+
+        self.decoder_pos_embed = nn.Parameter(torch.zeros(1, num_patches + 1, decoder_embed_dim),
+                                              requires_grad=False)  # fixed sin-cos embedding
+
+        self.decoder_blocks = nn.ModuleList([
+            Block(decoder_embed_dim, decoder_num_heads, mlp_ratio, qkv_bias=True, qk_scale=None, norm_layer=norm_layer)
+            for i in range(decoder_depth)])
+
+        self.decoder_norm = norm_layer(decoder_embed_dim)
+        self.decoder_pred = nn.Linear(decoder_embed_dim, out_patch_size ** 2 * out_chans, bias=True)  # decoder to patch
+        # --------------------------------------------------------------------------
+
+        self.initialize_weights()
+
+    def initialize_weights(self):
+        # initialization
+        # initialize (and freeze) pos_embed by sin-cos embedding
+        pos_embed = get_2d_sincos_pos_embed(self.pos_embed.shape[-1], int(self.patch_embed.num_patches ** .5),
+                                            cls_token=True)
+        self.pos_embed.data.copy_(torch.from_numpy(pos_embed).float().unsqueeze(0))
+
+        decoder_pos_embed = get_2d_sincos_pos_embed(self.decoder_pos_embed.shape[-1],
+                                                    int(self.patch_embed.num_patches ** .5), cls_token=True)
+        self.decoder_pos_embed.data.copy_(torch.from_numpy(decoder_pos_embed).float().unsqueeze(0))
+
+        # initialize patch_embed like nn.Linear (instead of nn.Conv2d)
+        w = self.patch_embed.proj.weight.data
+        torch.nn.init.xavier_uniform_(w.view([w.shape[0], -1]))
+
+        # timm's trunc_normal_(std=.02) is effectively normal_(std=0.02) as cutoff is too big (2.)
+        torch.nn.init.normal_(self.cls_token, std=.02)
+        torch.nn.init.normal_(self.mask_token, std=.02)
+
+        # initialize nn.Linear and nn.LayerNorm
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            # we use xavier_uniform following official JAX ViT:
+            torch.nn.init.xavier_uniform_(m.weight)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    def forward_encoder(self, x):
         x = self.patch_embed(x)
 
-        cls_tokens = self.cls_token.expand(B, -1, -1)
+        cls_tokens = self.cls_token.expand(x.shape[0], -1, -1)
         x = torch.cat((cls_tokens, x), dim=1)
         x = x + self.pos_embed
         x = self.pos_drop(x)
 
         for blk in self.blocks:
             x = blk(x)
-
         x = self.norm(x)
-        outcome = x[:, 0]
+        return x
 
-        return outcome
+    def forward_decoder(self, x):
+        x = self.decoder_embed(x)
+
+        x = x + self.decoder_pos_embed
+
+        for blk in self.decoder_blocks:
+            x = blk(x)
+        x = self.decoder_norm(x)
+
+        x = self.decoder_pred(x)
+        x = x[:, 1:, :]
+        return x
+
+    def forward(self, x):
+        x = self.forward_encoder(x)
+        x = self.forward_decoder(x)
+        return x
 
 
 def sprt_base_patch16_img192(**kwargs):
     model = SPRTransformer(
-        img_size=192, patch_size=16, in_chans=6, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
-        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+        img_size=192, patch_size=16, in_chans=2, embed_dim=768, depth=12, num_heads=12,
+        out_patch_size=8, out_chans=1, decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
+        mlp_ratio=4, qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     return model
