b5e2a6aedb65ade65e2ddb708de9448d0d3969a0
HiMyNameIsDavidKim <rkfka1401@gmail.com>
2024-08-22 15:07:45 +0900

    8/22 study update

--- CV/zJustTest.py
+++ CV/zJustTest.py
@@ -1,27 +1,8 @@
-import torch
-from matplotlib import pyplot as plt
+import shutil
+
+accuracies = [1,2,3,4,5,6,7,8,9,10]
+
+if len(accuracies) > 9:
+    print(accuracies[-10:-1])
 
 
-def acc_compare(self, *paths):
-    plt.figure(figsize=(4, 3), dpi=200)
-    for i, path in enumerate(paths):
-        checkpoint = torch.load(path, map_location='cpu')
-        self.epochs = checkpoint['epochs']
-        self.acc = checkpoint['accuracies']
-        ls_x = []
-        ls_y = []
-        for x, y in enumerate(self.acc):
-            ls_x.append(x)
-            ls_y.append(y)
-        plt.plot(ls_x, ls_y, label=f'Curve {i+1}')
-    plt.xlabel('Epochs', fontsize=10)
-    plt.ylabel('Validation Accuracy', fontsize=10)
-    plt.xticks(fontsize=8)
-    plt.yticks(fontsize=8)
-
-    ax = plt.gca()
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-
-    plt.legend()
-    plt.show()
